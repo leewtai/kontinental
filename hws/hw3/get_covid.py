6710eebe0c58d8615c45f65a0c4eded861689115
leewtai <leewtai@yahoo.com>
2022-01-31 12:54:50 -0500

    getting more maps

--- hws/hw3/get_covid.py
+++ hws/hw3/get_covid.py
@@ -1,3 +1,4 @@
+import numpy as np
 import re
 import logging
 import matplotlib.pyplot as plt
@@ -18,8 +19,6 @@ record_cap = 50000
 start_time = time.time()
 dats = []
 while OFFSET < 1:
-    logging.info('offset is at %d', OFFSET)
-    logging.info('time elapsed is %d seconds', (time.time() - start_time))
     resp = requests.get(
         url=URL,
         params={"$limit": record_cap, "$offset": OFFSET})
@@ -29,43 +28,78 @@ while OFFSET < 1:
     if len(dat) < record_cap:
         break
     OFFSET += record_cap
+    logging.info('offset is at %d', OFFSET)
+    logging.info('time elapsed is %d seconds', (time.time() - start_time))
 
 df = pd.concat(dats)
+logging.info('pulled %d records from health.gov', df.shape[0])
+
+personnel_cols = [col for col in df.columns
+                  if re.search('total_personnel', col)]
+pers_df = df.loc[:, personnel_cols].astype(float)
+pers_df[pers_df <0] = np.nan
+# Leave NAs to calc percentage
+df['total_personnel'] = pers_df.apply(sum, 1)
+df['perc_unvax'] = (
+    df.total_personnel_covid_vaccinated_doses_none_7_day.astype(float) / df.total_personnel
+    * 100)
 
 # Plot same graphic?
-condition = dat.collection_week == dat.collection_week.max()
-condition2 = dat.geocoded_hospital_address.apply(lambda x: isinstance(x, dict))
+target_week = df.collection_week.max()
+condition = df.collection_week == target_week
+condition2 = df.geocoded_hospital_address.apply(lambda x: isinstance(x, dict))
 assert condition.sum() > 4000
 
 # coordinates on geocoded_hospital_address
-sdf = dat.loc[condition & condition2,:]
+sdf = df.loc[condition & condition2,:]
 geoms = [shape(geojson) for geojson in sdf.geocoded_hospital_address]
-personnel_cols = [col for col in sdf.columns if re.search('total_personnel', col)]
-sdf.loc[:, personnel_cols] < 0
 
-total_staff = (
-    sdf.total_personnel_covid_vaccinated_doses_none_7_day
-    + sdf.total_personnel_covid_vaccinated_doses_one_7_day
-    + sdf.total_personnel_covid_vaccinated_doses_all_7_day)
-
-gdf = gpd.GeoDataFrame({'total_personnel_covid_vaccinated_doses_all_7_day': sdf.total_personnel_covid_vaccinated_doses_all_7_day, 'geometry': geoms})
-
-
+gdf = gpd.GeoDataFrame({
+    'total_personnel_reported': sdf.total_personnel.to_list(),
+    'perc_unvax': sdf.perc_unvax,
+    'beds_to_staff_ratio': (
+        sdf.total_personnel
+        / sdf.total_beds_7_day_avg.astype(float)).to_list(),
+    'geometry': geoms})
 
 not_conus = ['Hawaii', 'Alaska', 'Commonwealth of the Northern Mariana Islands',
              'Puerto Rico', 'American Samoa', 'Guam']
 # https://www.census.gov/geographies/mapping-files/time-series/geo/carto-boundary-file.html
 states = gpd.read_file("cb_2018_us_state_5m/cb_2018_us_state_5m.shp")
 conus = states.loc[states.NAME.apply(lambda x: x not in not_conus), :]
-min_bounds = conus.geometry.bounds.loc[:, ['minx', 'miny']].min()
-max_bounds = conus.geometry.bounds.loc[:, ['maxx', 'maxy']].max()
 
+conus_gdf = gdf.loc[(gdf.bounds.minx >= conus.bounds.minx.min())
+                    & (gdf.bounds.miny >= conus.bounds.miny.min())
+                    & (gdf.bounds.maxx <= conus.bounds.maxx.max())
+                    & (gdf.bounds.maxy <= conus.bounds.maxy.max()),:]
+
+markersize = 100 * (conus_gdf.total_personnel_reported
+                    / conus_gdf.total_personnel_reported.max())
 base = conus.plot(color='#AAAAAA')
-gdf.plot(ax=base, marker='o', color='red', markersize=gdf.)
-#plt.xlim((min_bounds.minx, max_bounds.maxx))
-#plt.ylim((min_bounds.miny, max_bounds.maxy))
-plt.savefig('states.png')
+conus_gdf.plot(ax=base, marker='o', markersize=markersize,
+               cmap='Greens', c=conus_gdf.perc_unvax)
+plt.title('Unvaccinated hospital personnel on week {}'.format(target_week[:10]))
+plt.savefig('reproduce_unvaccinated.png')
+plt.close()
+
+
+not_inf = np.logical_not(np.isinf(conus_gdf.beds_to_staff_ratio))
+markersize = 100 * (conus_gdf.beds_to_staff_ratio
+                    / conus_gdf.beds_to_staff_ratio[not_inf].max())
+base = conus.plot(color='#AAAAAA')
+conus_gdf.plot(ax=base, marker='o', markersize=markersize,
+               color='green')
+plt.title('Total beds to staff ratio on week {}'.format(target_week[:10]))
+plt.savefig('bed_to_staff_ratio.png')
 plt.close()
 
 
 
+# Group by the hospital id, see weekly data
+hos_grp = df.groupby('hhs_ids')
+
+TARGET = ''
+for grp, ind in hos_grp.groups.items():
+    # grp, ind = next(iter(hos_grp.groups.items()))
+    sdf = df.loc[ind, ['collection_week', TARGET, personnel_cols]]
+    sdf.sort_values('collection_week', inplace=True)
